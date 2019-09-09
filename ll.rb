2cd7508b483ac68b4e6c3cc50e66c14f96ea69bf
Adebola <onedebos@gmail.com>
2019-09-09 13:59:12 +0100

    fix lint errors

--- ll.rb
+++ ll.rb
@@ -1,54 +1,54 @@
+# frozen_string_literal: true
+
+# Node BluePrint
 class Node
-    attr_accessor :val, :next_node
-  def initialize(val,next_node = nil)
+  attr_accessor :val, :next_node
+  def initialize(val, next_node = nil)
     @val = val
     @next_node = next_node
   end
 end
 
+# LinkedList BluePrint
 class LinkedList
-
-  #add to the end of the list
+  # add to the end of the list
   def push(value)
-    if @head==nil
-        @head = Node.new(value)
-        current_node = @head
-    else 
-        current_node = @head
-        while(current_node.next_node!=nil)
-            current_node = current_node.next_node
-        end
-        current_node.next_node = Node.new(value)  
-        current_node = current_node.next_node
+    if @head.nil?
+      @head = Node.new(value)
+      current_node = @head
+    else
+      current_node = @head
+      current_node = current_node.next_node until current_node.next_node.nil?
+      current_node.next_node = Node.new(value)
+      current_node = current_node.next_node
     end
     current_node
   end
 
-
-  #method removes item at the end of the list
+  # method removes item at the end of the list
   def pop
     current_node = @head
     if current_node.next_node.nil?
-       @head == nil
+      @head.nil?
     else
-      while(current_node.next_node)
+      while current_node.next_node
         previous = current_node
         current_node = current_node.next_node
       end
-    previous.next_node = nil
+      previous.next_node = nil
     end
   end
 
-   #method removes item at the beginning of the list
-   def shift
-     current_node = @head
-     @head = current_node.next_node
-   end
+  # method removes item at the beginning of the list
+  def shift
+    current_node = @head
+    @head = current_node.next_node
+  end
 
-   #method to add at the beginning of the list
+  # method to add at the beginning of the list
   def unshift(val)
     if @head.nil?
-      @head = @tail =  Node.new(val)
+      @head = @tail = Node.new(val)
     else
       new_node = Node.new(val)
       new_node.next_node = @head
@@ -56,29 +56,27 @@ class LinkedList
     end
   end
 
-  #method to get value at a given index
+  # method to get value at a given index
   def get_val_at_index(index)
     if index_valid?(index)
       current_node = @head
       i = 0
       while i < index
         current_node = current_node.next_node
-        i+=1
+        i += 1
       end
-    current_node.val
-    else
-      nil
+      current_node.val
     end
   end
 
-  #method to add at a given index
-  def add_at_index(index,value)
-    #if index is 0, add to the beginning of the list else
-    #traverse the list till you get to index
-    #create a new node with the value
-    #set the next node of the new node to the next node of the current node
-    #set the next node of the previous node to the new node
-    if index == 0
+  # method to add at a given index
+  def add_at_index(index, value)
+    # if index is 0, add to the beginning of the list else
+    # traverse the list till you get to index
+    # create a new node with the value
+    # set the next node of the new node to the next node of the current node
+    # set the next node of the previous node to the new node
+    if index.zero?
       unshift(value)
     elsif index_valid?(index)
       new_node = Node.new(value)
@@ -87,21 +85,19 @@ class LinkedList
       while i < index
         previous_node = current_node
         current_node = current_node.next_node
-        i+=1
+        i += 1
       end
       new_node.next_node = current_node
       previous_node.next_node = new_node
-    else
-      nil
     end
   end
 
-  def set(index,value)
+  def set(index, value)
     current_node = @head
     i = 0
     while i < index
       current_node = current_node.next_node
-      i+=1
+      i += 1
     end
     current_node.val = value
   end
@@ -109,14 +105,13 @@ class LinkedList
   def remove(index)
     current_node = @head
     i = 0
-    while i<index
+    while i < index
       previous_node = current_node
       current_node = current_node.next_node
-      i+=1
+      i += 1
     end
     temp_node = current_node.next_node
     previous_node.next_node = temp_node
-    current_node = nil
   end
 
   def reverse_list
@@ -124,45 +119,41 @@ class LinkedList
     @head = @tail
     @tail = node
     prev = nil
-    
-    for i in (0..length)
-       next_item = node.next_node
-       node.next_item = prev
-       prev = node
-       node = next_item
 
+    (0..length).each do |_i|
+      next_item = node.next_node
+      node.next_item = prev
+      prev = node
+      node = next_item
     end
-
   end
 
-  
-  #helper methods
-  
-  #method checks if index exists
+  # helper methods
+
+  # method checks if index exists
   def index_valid?(index)
-    index > -1 && index != length ? true : false 
+    index > -1 && index != length ? true : false
   end
 
-  #method to get the length of the list
+  # method to get the length of the list
   def length
     current_node = @head
     count = 1
-    while current_node.next_node!=nil
+    until current_node.next_node.nil?
       current_node = current_node.next_node
-      count+=1
+      count += 1
     end
     count
   end
-
-end #end of class
+end
 
 list = LinkedList.new
 list.push(3)
 list.push(4)
 list.push(5)
 list.push(6)
-list.set(2,20)
-list.reverse_list
+list.set(2, 20)
+# list.reverse_list
 # list.add_at_index(0,24)
 
 # list.pop
@@ -173,11 +164,9 @@ list.reverse_list
 
 p list
 
-=begin
-methods to implement in linked list
-insert to the end
-add_at
-delete
-
-    
-=end
+# methods to implement in linked list
+# insert to the end
+# add_at
+# delete
+#
+#
--- stacks.rb
+++ stacks.rb
@@ -1,46 +1,47 @@
+# frozen_string_literal: true
+
+# Create Node
 class Node
-    attr_accessor :val, :next_node
-    def initialize(val, next_node = nil)
-        @val = val
-        @next_node = next_node
-    end
+  attr_accessor :val, :next_node
+  def initialize(val, next_node = nil)
+    @val = val
+    @next_node = next_node
+  end
 end
 
-
+# Create Stack blueprint
 class Stack
-#stacks are LIFO (last-in-first-out)
-#elements are always added and removed from the top 
-#of the stack
+  # stacks are LIFO (last-in-first-out)
+  # elements are always added and removed from the top
+  # of the stack
 
-   #add data to the stack
+  # add data to the stack
   def push(value)
-    if @top == nil
+    if @top.nil?
       @top = Node.new(value)
     else
       new_node = Node.new(value)
       new_node.next_node = @top
       @top = new_node
     end
-   end
-   
-   #remove data from the stack
-   def pop
-     top_node = @top
-     @top = top_node.next_node
-     top_node = nil 
-   end
+  end
 
-   #check if stack is empty
-   def is_empty?
-     if @top == nil
-       true
-     else
-       false
-     end 
-   end
+  # remove data from the stack
+  def pop
+    top_node = @top
+    @top = top_node.next_node
+  end
 
+  # check if stack is empty
+  def empty?
+    if @top.nil?
+      true
+    else
+      false
+    end
+  end
 end
-#written by Debs
+# written by Debs
 
 stack = Stack.new
 stack.push(3)
@@ -48,6 +49,6 @@ stack.push(4)
 stack.push(5)
 stack.push(6)
 stack.pop
-puts stack.is_empty?
+puts stack.empty?
 
 p stack
